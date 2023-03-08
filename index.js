9af02b39bd2db521cf24907edf59942e8c4f9e63
Oignnon - <td665pro@pm.me>
2023-03-08 14:25:28 +0100

    Unsure how to implement this locally!

---
+++ index.js
@@ -0,0 +1,34 @@
+// import MONGO_USERNAME from "./key.js";
+// import MONGO_PASSWORD from "./key.js";
+
+
+import app from "./server.js";
+import mongodb from "./mongodb";
+//import ReviewsDAO from "./dao/reviewsDAO.js";
+
+const MongoClient = mongodb.MongoClient;
+const mongo_username = process.env["MONGO_USERNAME"];
+const mongo_password = process.env["MONGO_PASSWORD"];
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.4jpujzw.mongodb.net/?retryWrites=true&w=majority`;
+
+//Using `` let's you use variables in the string
+
+const port = 8000;
+
+MongoClient.connect(
+    uri,
+    {
+        maxPoolSize: 50, 
+        wtimeoutMS: 2500,
+        useNewUrlParser: true
+    })
+    .catch(err => {
+        console.error(err.stack)
+        process.exit(1)
+    })
+    .then(async client => {
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`)
+        })
+    })
+
