61187ec9ded23e5991a03868c21376936fdd5336
Oignnon - <td665pro@pm.me>
2023-03-16 12:43:06 +0100

    Trying to get backend working locally but proving to be tricky. Even with user and pass hard coded stuill not having any luck with POST and PUT

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
-//Create express server, listen on port 5000
-//Web framework for node.js
-//Using cores
-
-import express from "express";
-import cors from "cors";
+import express from "express"
+import cors from "cors"
 import reviews from "./api/reviews.route.js"
 
 const app = express()
@@ -12,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/v1/reviews", reviews)
-app.use("*", (req, res) =>
-res.status(404).json({error: "not found"}))
+app.use("*", (req, res) => 
+res.status(404). json({error: "not found"}))
 
 export default app
